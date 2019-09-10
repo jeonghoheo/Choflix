883c59ec547554b8ec68d0cb238da10c748848e6
jeonghoheo <heojeongho1992@gmail.com>
2019-09-10 22:33:48 +0900

    Updates

---
+++ precache-manifest.00d1e238b082fde579b7518e9c4a1a02.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "48b37b0454d0f2e266393cc5c9772d19",
+    "url": "/choflix/index.html"
+  },
+  {
+    "revision": "baf0f266af999a7a379a",
+    "url": "/choflix/static/js/2.9726cbe1.chunk.js"
+  },
+  {
+    "revision": "d252ffdd0fed627425e1",
+    "url": "/choflix/static/js/main.b4084c0b.chunk.js"
+  },
+  {
+    "revision": "f690533a86d1167a5792",
+    "url": "/choflix/static/js/runtime~main.26717052.js"
+  },
+  {
+    "revision": "87a0eb6a89037f2aa1b57bba7d2db7e1",
+    "url": "/choflix/static/media/noPosterSmall.87a0eb6a.png"
+  }
+]);
